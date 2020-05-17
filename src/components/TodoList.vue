f253c46e5c0100d278a4dcb6590302a48ae3522c
Dexter Dagorne <dj.dagorne@gmail.com>
2020-05-16 20:37:35 -0400

    throwin the whole thing up on github at once

---
+++ src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+  <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+  <!-- declare the key in a v-for loop as a variable in the declaration of your loop. 
+  in this case its index -->
+  <todo 
+  v-on:delete-todo="deleteTodo" 
+  v-on:complete-todo="completeTodo"
+  v-for="(todo, index) in todos" 
+  v-bind:todo="todo" 
+  v-bind:key="index"></todo>
+</div>
+</template>
+
+<script type="text/javascript">
+import Todo from './Todo'
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo,
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    completeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos[todoIndex].done = !this.todos[todoIndex].done;
+    }
+  }
+};
+</script>
+<style>
+
+</style>
